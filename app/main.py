06f19c5525f3eaacbece78a5d656fbec291cae24
softshipper <anujit.marty@hispeed.ch>
2024-12-30 15:07:10 +0100

    cm

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 
 app.include_router(users.router)
